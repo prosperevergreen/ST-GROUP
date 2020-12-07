fcc2735c4bd1bbb9e6e888b44f5fc149c378d307
prosperevergreen <prosperevergreen@gmail.com>
2020-12-07 19:59:38 +0200

    add more test

--- test/src/eq.spec.js
+++ test/src/eq.spec.js
@@ -1,24 +1,21 @@
-import pkg from 'chai';
-import chai from 'chai';
-const {expect} = pkg;
+import pkg from "chai";
+import chai from "chai";
+const { expect } = pkg;
 const assert = chai.assert;
 
 import eq from "../../src/eq.js";
 
-describe ("equality Unit-Testing",()=>{
-    it("Checking two elements of same type", function(){
-    const result = eq('a','a');
-    assert.equal(result,true);
-    });
-    it("Checking two elements of same type 1",function(){
-        const result = eq(1,1);
-        assert.equal(result,true);
-        
-    });
-    it("Checking two elements with different type "),function(){
-        const result = eq( 'a' , parseInt ('a') );
-        assert.equal(result,false);
-        
-}
-
+describe("equality Unit-Testing", () => {
+	it("Checking two elements of same type", function () {
+		const result = eq("a", "a");
+		assert.equal(result, true);
+	});
+	it("Checking two elements of same type 1", function () {
+		const result = eq(1, 1);
+		assert.equal(result, true);
+	});
+	it("Checking two elements with different type ", function () {
+		const result = eq("a", parseInt("a"));
+		assert.equal(result, false);
+	});
 });
