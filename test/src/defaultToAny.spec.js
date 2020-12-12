976b52a9c676fb48d4ba31b6ebc7821985f2c25e
prosperevergreen <prosperevergreen@gmail.com>
2020-12-12 03:43:33 +0200

    update test coverage

--- test/src/defaultToAny.spec.js
+++ test/src/defaultToAny.spec.js
@@ -1,25 +1,23 @@
-import pkg from 'chai';
-const {expect} = pkg ;
+import pkg from "chai";
+const { expect } = pkg;
 
 import defaultToAny from "../../src/defaultToAny.js";
 
-describe("unit testing - DefaultToAny",()=>{
-    it("Normal cases", function(){
-    const result = defaultToAny(2,5,6);
-    expect(result).to.equal(2);
-    });
-    it("False cases 1",function(){
-    const result1 = defaultToAny(undefined,5,10);
-    expect(result1).to.equal(5);
-    });
-    it("False case 2",function(){
-        const result2 = defaultToAny(null,undefined,10);
-        expect(result2).to.equal(10);
-        });
-        it("False case 3",function(){
-            const result2 = defaultToAny(null,undefined,NaN);
-            expect(result2).to.deep.equal(NaN);
-            });
-    
-
-    });
+describe("unit testing - DefaultToAny", () => {
+	it("Normal cases", function () {
+		const result = defaultToAny(2, 5, 6);
+		expect(result).to.equal(2);
+	});
+	it("False cases 1", function () {
+		const result1 = defaultToAny(undefined, 5, 10);
+		expect(result1).to.equal(5);
+	});
+	it("False case 2", function () {
+		const result2 = defaultToAny(null, undefined, 10);
+		expect(result2).to.equal(10);
+	});
+	it("False case 3", function () {
+		const result2 = defaultToAny(null, undefined, NaN);
+		expect(result2).to.deep.equal(NaN);
+	});
+});
--- test/src/difference.spec.js
+++ test/src/difference.spec.js
@@ -1,22 +1,17 @@
-import pkg from 'chai';
-const {expect} = pkg;
+import pkg from "chai";
+const { expect } = pkg;
 
 import difference from "../../src/difference.js";
 
-describe("Difference",()=>{
-    it("Return type 1", function(){
-    const result = difference([3,4],[2,3]);
-    
-    expect(result).to.deep.equal([4]);
-    });
-    it("Return type error",function(){
-        const result = difference([3,4],[2,3,1]);
-       
-        expect(result).to.deep.equal([4]);
-        
-     });
-    
-        
+describe("Difference", () => {
+	it("Return type 1", function () {
+		const result = difference([3, 4], [2, 3]);
 
+		expect(result).to.deep.equal([4]);
+	});
+	it("Return type error", function () {
+		const result = difference(2, [2, 3, 1]);
 
-    });
+		expect(result).to.deep.equal([4]);
+	});
+});
